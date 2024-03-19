49fceb990e6323a2b3617dc6af6ad37cf9caaaf2
ldduc123 <ducld0510@gmail.com>
2024-03-20 01:45:03 +0700

    Added h1 tag

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
